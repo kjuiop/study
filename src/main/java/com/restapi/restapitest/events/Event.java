616f4f03ba372704e8f8cb2d09c5c26f23eff526
kjuiop <copying_y@naver.com>
2020-03-22 20:53:23 +0900

    POST /api/events return 201

--- src/main/java/com/restapi/restapitest/events/Event.java
+++ src/main/java/com/restapi/restapitest/events/Event.java
@@ -2,25 +2,32 @@ package com.restapi.restapitest.events;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode(of="id")
+@Entity
 public class Event {
 
+    @Id
+    @GeneratedValue
     private Integer id;
+
     private String name;
     private String description;
     private LocalDateTime beginEnrollmentDateTime;
     private LocalDateTime closeEnrollmentDateTime;
     private LocalDateTime beginEventDateTime;
     private LocalDateTime endEventDateTime;
-    private String location;
-    private int basePrice;
-    private int maxPrice;
+    private String location;    // optional
+    private int basePrice;    // optional
+    private int maxPrice;    // optional
     private int limitOfEnrollment;
     private boolean offLine;
     private boolean free;
+
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus;
 
 }
